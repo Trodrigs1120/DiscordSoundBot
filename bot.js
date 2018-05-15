b5bf7e731b5379638934de055ba6e26a4549d935
unknown <Trodrigs@yahoo.com>
2018-05-15 15:08:03 -0400

    initial upload of soundboard bot

---
+++ bot.js
@@ -0,0 +1,50 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const auth = require("./auth.json")
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+client.on("message", (message) => {
+  if (message.content.startsWith("!help")) {
+    message.channel.send("The bot is still a work in progress but It currently has a few commands");
+    message.channel.send('"!joinme" will join the bot into your channel');
+    message.channel.send('"!goaway" will remove the bot from your channel');
+    message.channel.send('"!dunk1" and "!wimmy" will play the sound bite');
+  }
+
+  if (message.content.startsWith("!joinme")) {
+    const channel = message.member.voiceChannel;
+    channel.join()
+  }
+
+  if (message.content.startsWith("!goaway")) {
+    const channel = message.member.voiceChannel;
+    channel.leave();
+  }
+
+  if (message.content.startsWith("!dunk1")) {
+    const channel = message.member.voiceChannel;
+    channel.join()
+  .then(connection => {
+    const broadcast = client
+    .createVoiceBroadcast()
+    .playFile('./assets/Dunk1.mp3');
+  const dispatcher = connection.playBroadcast(broadcast);
+  })
+}
+  if (message.content.startsWith("!wimmy")) {
+    const channel = message.member.voiceChannel;
+    channel.join()
+  .then(connection => {
+    const broadcast = client
+    .createVoiceBroadcast()
+    .playFile('./assets/wimmy.mp3');
+  const dispatcher = connection.playBroadcast(broadcast);
+  })
+  .catch(console.error);
+  }
+
+
+});
+client.login(auth.token);
+//remove this shit before you upload to github
